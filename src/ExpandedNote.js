d34ef5c0cf21674333dfdbbeeeee3d01c1f1a86f
Bruno Mota <bmota2233@gmail.com>
2020-09-03 11:00:36 -0400

    add goback after expanded note is deleted

--- src/ExpandedNote.js
+++ src/ExpandedNote.js
@@ -4,9 +4,27 @@ import NotefulContext from './NotefulContext';
 export default class ExpandedNote extends Component{
   static contextType = NotefulContext;
 
+  static defaultProps = {
+    onDeleteNote: () => {}
+  }
+
+  handleButtonClick = () => {
+    console.log(this.context)
+    fetch(`http://localhost:9090/notes/${this.props.match.params.noteId}`, {
+      method: 'DELETE'
+    }).then(() => {
+      this.context.deleteNote(this.props.match.params.noteId);
+      this.props.onDeleteNote(this.props.match.params.noteId);
+    }).catch(error => {
+      console.log(error);
+    });
+    this.props.history.goBack();
+  }
+
   goBack = () => {
     this.props.history.goBack();
   }
+  
   render()
   {
     let note = this.context.notes.find(note => note.id === this.props.match.params.noteId);
@@ -17,7 +35,7 @@ export default class ExpandedNote extends Component{
         <h2>{note.name}</h2>
         <h3>{note.modified}</h3>
         <p>{note.content}</p>
-        <button onClick={() => this.context.deleteNote} className="note-buttton-delete">DELETE</button>
+        <button onClick={this.handleButtonClick} className="note-buttton-delete">DELETE</button>
         <hr/>
         <h2>Current folder: {folder.name}</h2>
         <button onClick={this.goBack}>Go back!</button>
